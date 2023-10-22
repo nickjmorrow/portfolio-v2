9639f4510d9cb563d85e9e6c6fe643dbf02fbba7
Nicholas Morrow <njmorrow95@gmail.com>
2023-10-22 13:16:23 -0500

    Remove in progress placeholder.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { SiteInProgress } from 'src/modules/core/SiteInProgress';
 import { Layout } from 'src/modules/main/Layout';
 import { theme } from 'src/modules/theming/theme';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 
 function App() {
-    const showSitePlaceholder =
-        import.meta.env.PROD || localStorage.getItem('showSiteAnyways');
-
     return (
         <ThemeProvider theme={theme}>
-            {showSitePlaceholder ? <SiteInProgress /> : <Layout />}
+            <Layout />
         </ThemeProvider>
     );
 }
